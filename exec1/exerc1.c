8f0883f31716015636ad58fa3bc418cf29653888
Felipe Dalla <fedallavhotmailcom@gmail.com>
2026-03-01 12:42:05 -0300

    Add files via upload
    Envio das atividades e arquivos gerados.

---
+++ exec1/exerc1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define NOME "Felipe"
+
+int main(){
+
+    printf ("Meu nome eh %s", NOME);
+    return 0;
+
+}
---
+++ exec6/exec6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+ 
+    char letra = 'g';
+
+    printf("O caractere escolhido foi: %c\n", letra);
+    printf("O valor correspondente na tabela ASCII e: %d\n", letra);
+
+    return 0;
+}
